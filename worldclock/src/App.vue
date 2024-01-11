9db1ffaf92b9c5d598c7fcd9a2f74d5314d95139
Philipp Müller <philipp.mueller@hb-capital.de>
2024-01-11 15:37:35 +0100

    feat: adding video as background

--- worldclock/src/App.vue
+++ worldclock/src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
+import BackgroundVideo from "./components/BackgroundVideo.vue";
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
 import Footer from "./components/Footer.vue";
-
 </script>
 
 <template>
   <div>
+    <BackgroundVideo></BackgroundVideo>
     <Header></Header>
     <Main></Main>
     <Footer></Footer>
---
+++ worldclock/src/components/BackgroundVideo.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div>
+    <iframe
+      width="1470"
+      height="920"
+      src="https://www.youtube.com/embed/eEpEeyqGlxA"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    ></iframe>
+  </div>
+</template>
