69743ff3dde731629de20583c70217ed174bd1b1
Philipp Müller <philipp.mueller@hb-capital.de>
2024-01-12 11:55:57 +0100

    solved: video is now background

--- worldclock/src/components/BackgroundVideo.vue
+++ worldclock/src/components/BackgroundVideo.vue
@@ -1,13 +1,9 @@
 <script setup></script>
 
 <template>
-  <div>
-    <iframe
-      width="1470"
-      height="920"
-      src="https://www.youtube.com/embed/eEpEeyqGlxA"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    ></iframe>
+  <div class="background__video">
+    <video playsinline autoplay muted loop>
+      <source src="background_RotatingEarth.mp4" type="video/mp4" />
+    </video>
   </div>
 </template>
